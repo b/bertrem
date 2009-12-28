c40129025f137c897d531d4d1cf6ce4ebbc9fe08
b <b@fastip.com>
2009-12-27 22:28:32 -0800

    Removed dead debug code

--- lib/bertem/bertrpc.rb
+++ lib/bertem/bertrpc.rb
@@ -2,7 +2,7 @@ module EventMachine
   module Protocols
 
     # = Example
-    #EM.run{
+    # EM.run{
     #   svc = EM::Protocols::BERTRPC.connect('localhost', 9999)
     #
     #   req = svc.call.calc.add(1, 2)
@@ -36,17 +36,14 @@ module EventMachine
       end
 
       def post_init
-				@response_data = ""
 				@requests = []
       end
 
       def receive_data(bert_response)
-        @response_data << bert_response
-        
-        raise ::BERTRPC::ProtocolError.new(::BERTRPC::ProtocolError::NO_HEADER) unless @response_data.length > 4
-        len = @response_data.slice!(0..3).unpack('N').first # just here to strip the length header
-        raise ::BERTRPC::ProtocolError.new(::BERTRPC::ProtocolError::NO_DATA) unless @response_data.length > 0
-        @response = decode_bert_response(@response_data)
+        raise ::BERTRPC::ProtocolError.new(::BERTRPC::ProtocolError::NO_HEADER) unless bert_response.length > 4
+        len = bert_response.slice!(0..3).unpack('N').first # just here to strip the length header
+        raise ::BERTRPC::ProtocolError.new(::BERTRPC::ProtocolError::NO_DATA) unless bert_response.length > 0
+        @response = decode_bert_response(bert_response)
         @requests.pop.succeed(@response)
       end
       
